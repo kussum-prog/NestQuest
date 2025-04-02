2e8b5a9146c908a87f81759ce825946f5da39e1c
Kussum-prog <kussum789@gmail.com>
2025-04-02 09:19:02 +0530

    admin panel setup

--- frontend/src/Components/Home/Header.jsx
+++ frontend/src/Components/Home/Header.jsx
@@ -15,17 +15,23 @@ return (
 
 {/* Navbar Links */}
 <div className="flex gap-5">
-<Link to="/" className="text-white  flex items-center justify-center p-1 hover:text-amber-300 transition duration-300">
+<Link to="/" className="text-white  flex items-center group justify-center p-1 hover:text-amber-300 transition duration-300">
 Home
+<span className="absolute h-0.5 bg-yellow-400 transition-all  group-hover:w-15 group-hover:bottom-0.5"></span>
 </Link>
-<Link to="/Property" className="text-white flex items-center justify-center p-1 hover:text-amber-300 transition duration-300">
+<Link to="/Property" className="text-white flex items-center group justify-center p-1 hover:text-amber-300 transition duration-300">
 Post Your Property
+<span className="absolute h-0.5 bg-yellow-400 transition-all group-hover:w-40 group-hover:bottom-2"></span>
 </Link>
-<Link to="/SignUp" className="text-white flex items-center justify-center p-1 hover:text-amber-300 transition duration-300">
+<Link to="/SignUp" className="text-white flex items-center group justify-center p-1 hover:text-amber-300 transition duration-300">
 SignUp
+<span className="absolute h-0.5 bg-yellow-400 transition-all  group-hover:w-15 group-hover:bottom-2"></span>
 </Link>
-<Link to="/LogIn" className="text-black text-md text-bold flex items-center justify-center  rounded-md p-2 bg-yellow-300  border-2 uppercase  hover:text-amber-300 transition duration-300 mr-4  font-poppins">
+<Link to="/LogIn" className="text-black text-md text-bold flex items-center justify-center  rounded-md p-2 bg-yellow-300  border-2 uppercase  hover:text-black hover:bg-slate-300  transition duration-300 mr-4  font-poppins">
 LogIn
+</Link>
+<Link to="/Profile" className="text-black text-md text-bold flex items-center justify-center  rounded-md p-2 bg-yellow-300  border-2 uppercase  hover:text-black  hover:bg-slate-300 transition duration-300 mr-4  font-poppins">
+Profile
 </Link>
 </div>
 </div>
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Hero from '../Components/Home/Hero';
 import Header from "../Components/Home/Header";
-import Cards from '../Components/Cards';
+
 import Footer from '../Pages/Footer';
 import Use from './Use';
 import RentOutProperty from './RentOutProperty';
+import Cards from '../Components/Home/Cards';
 
 
 
