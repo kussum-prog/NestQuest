2aad89bef9d96900435a532fe2ef71c7b2370d98
Kussum-prog <kussum789@gmail.com>
2025-04-15 10:07:59 +0530

    delete api

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,6 +15,7 @@ createRoot(document.getElementById('root')).render(
    <BrowserRouter> {/* Wrap App with BrowserRouter */}
       <App />
       <ToastContainer
+      
         position="top-center"
         autoClose={3000} // Auto close in 3 seconds
         hideProgressBar={false}
